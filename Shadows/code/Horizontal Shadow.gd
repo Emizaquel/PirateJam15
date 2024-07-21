d0f2ebba1aa8e426dd2db182340f4876167aec29
Emizaquel <Emizaquel@gmail.com>
2024-07-21 18:32:42 +0800

    added previsualization for shadow development

--- Shadows/code/Horizontal Shadow.gd
+++ Shadows/code/Horizontal Shadow.gd
@@ -1,19 +1,16 @@
 extends Area2D
 
-@onready var collider = $collider
-@onready var polygon = $visual
-@export var daylight:Timer # update this to the actual timer object
-@export var verticies:Array[Vector2] = []
+@onready var collider = $"collider shape"
+@onready var polygon = $".."
+@onready var daylight:Timer = polygon.daylight # update this to the actual timer object
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	collider.polygon = verticies
-	polygon.polygon = verticies
-
+	collider.polygon = polygon.polygon
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var progress = (1 + (daylight.time_left/daylight.wait_time))*PI
-	rotation = progress
-	skew = 0-progress
+	polygon.rotation = progress
+	polygon.skew = 0-progress
 	pass
